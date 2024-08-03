00bf3dad1251239b71fec0f521ad7e82b95f3ec4
Deepanshu <deepanshubawaliya@gmail.com>
2024-08-03 17:02:05 +0530

    Contact Page completed!

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className='flex border border-black p-3 bg-black justify-center'>
+            <div className='flex border border-black p-3 bg-black justify-center mt-3'>
                 <span className='text-white'>@ 2024 ArcadeCalc - All Rights Reserved.</span>
             </div>
         </>
