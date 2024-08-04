12b791700ced5d1e52088881e536dca514da3d98
Deepanshu <deepanshubawaliya@gmail.com>
2024-08-04 10:29:41 +0530

    fixed bugs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <>
 
             <nav className='bg-blue-500 p-3 text-white flex justify-between mb-5'>
-                <span className='text-2xl font-bold'>Arcade Helper</span>
+                <span className='text-2xl font-bold'>Arcade Calc</span>
                 <ul className='flex text-xl mt-1'>
                     <li className='mr-9 cursor-pointer hover:underline'><Link to="/">Home</Link></li>
                     <li className='mr-9 cursor-pointer hover:underline'><Link to="/calculate">Calculate Points</Link></li>
