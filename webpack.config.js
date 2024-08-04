f7b27bf2b31bceee85c977c83f5e27f41540e4f8
Deepanshu <deepanshubawaliya@gmail.com>
2024-08-04 11:44:58 +0530

    fixed bugs

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+    // Other webpack configuration options...
+    resolve: {
+        fallback: { "path": require.resolve("path-browserify") }
+    }
+};
