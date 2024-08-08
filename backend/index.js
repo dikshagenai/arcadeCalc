1596fdade0597185d0af3507fc15160c637a6fd0
Deepanshu <deepanshubawaliya@gmail.com>
2024-08-08 10:47:18 +0530

    Backend sleeping issue fixed! - No late responses

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const Arcade = require("./api")
 const cors = require("cors")
+const axios = require('axios');
+
 
 app.use(express.json());
 app.use(cors())
+
+app.get('/', async (req, res) => {
+    res.status(200).send("Server is Live!")
+})
+
+
 app.post('/calculate', async (req, res) => {
     const publicUrl = req.body.publicUrl; // Assuming you send data as JSON
     try {
@@ -19,6 +27,21 @@ app.post('/calculate', async (req, res) => {
 
 });
 
+const url = `https://arcadecalc.onrender.com`; // Replace with your Render URL
+const interval = 60000; // Interval in milliseconds (1 minute)
+
+//Reloader Function
+function reloadWebsite() {
+    axios.get(url)
+        .then(response => {
+            console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+        })
+        .catch(error => {
+            console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+        });
+}
+
+setInterval(reloadWebsite, interval);
 
 
 
