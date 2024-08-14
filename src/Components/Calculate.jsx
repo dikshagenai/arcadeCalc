b6b1010bc47403fa3e93b5cd779ad1abd55e13e1
Deepanshu <deepanshubawaliya@gmail.com>
2024-08-14 12:11:27 +0530

    empty commit

--- src/Components/Calculate.jsx
+++ src/Components/Calculate.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react'
 import Alert from "./Alert"
 import LastUpdated from './LastUpdated'
+import { motion } from 'framer-motion'
+
 
 const Calculate = () => {
 
@@ -150,7 +152,7 @@ const Calculate = () => {
                             {/* SHOW WHEN CALCULATING  */}
                             <div className='flex justify-center items-center'>
                                 <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
                                 <span className='text-sm'>Processing...</span>
@@ -158,7 +160,7 @@ const Calculate = () => {
                         </>
                             : <>
                                 {/* SHOW WHEN NOT CALCULATING  */}
-                                <div div className='flex justify-center items-center'>
+                                <div className='flex justify-center items-center'>
                                     <span className='text-sm'>Calculate Points</span>
                                 </div>
                             </>
