dd063d57fb11ea8b246e56c1c2288e566d757ebc
Deepanshu <deepanshubawaliya@gmail.com>
2025-01-12 13:24:11 +0530

    fixed the server delay

--- buildTime.js
+++ buildTime.js
@@ -1,12 +1,12 @@
 // config.js
 
 // Hosted on main account
-// const SERVER = 'https://arcadecalc.onrender.com';
+const SERVER = 'https://arcadecalc.onrender.com';
 
 // Hosted on secondary account
 // const SERVER = 'https://arcadecalc-f7cj.onrender.com';
 
 // Testing Url
-const SERVER = 'http://localhost:5000';
+// const SERVER = 'http://localhost:5000';
 
 module.exports = SERVER;
