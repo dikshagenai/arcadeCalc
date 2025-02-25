d0ac56e0af3eb834878d4b5fcd2f11c8d81535ba
Deepanshu <deepanshubawaliya@gmail.com>
2025-02-25 21:40:22 +0530

    FIxed buildTime

--- buildTime.js
+++ buildTime.js
@@ -1,12 +1,12 @@
 // config.js
 
 // Hosted on main account
-const SERVER = 'https://arcadecalc.onrender.com';
+// const SERVER = 'https://arcadecalc.onrender.com';
 
 // Hosted on secondary account
-// const SERVER = 'https://arcadecalc-f7cj.onrender.com';
+const SERVER = 'https://arcadecalc-f7cj.onrender.com';
 
 // Testing Url
 // const SERVER = 'http://localhost:5000';
 
-module.exports = SERVER;
+module.exports = SERVER;https://arcadecalc.netlify.app/
